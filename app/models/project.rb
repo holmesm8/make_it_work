b338503cee450f32cfc0169e392b59a02e250b3f
Matt Holmes <holmesm8@gmail.com>
2020-02-07 11:14:09 -0700

    Finish user story 3

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,10 @@
-class Project <ApplicationRecord
+class Project < ApplicationRecord
   validates_presence_of :name, :material
   belongs_to :challenge
   has_many :contestant_projects
   has_many :contestants, through: :contestant_projects
+
+  def number_of_contestants
+    self.contestants.count
+  end
 end
