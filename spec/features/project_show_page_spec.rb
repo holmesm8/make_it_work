d79efc2b86bf528db852b7a4792912dc0dbefe39
Matt Holmes <holmesm8@gmail.com>
2020-02-07 10:08:03 -0700

    Rework testing for user story 1. Show view now shows all 3 requirements

--- spec/features/project_show_page_spec.rb
+++ spec/features/project_show_page_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe "When a user visits a project's show page" do
-  it "it sees that project's name and material" do
+  it "it sees that project's name, material, and theme of the challenge" do
     challenge1 = Challenge.create!(theme: "Apartment Furnishings", project_budget: 10)
-    project1 = Project.create!(name: 'Litfit', material: "Lamp Shade", challenge_id: challenge1.id)
+    project1 = challenge1.projects.create!(name: 'Litfit', material: "Lamp Shade")
 
     visit "/projects/#{project1.id}"
 
-
-    expect(page).to have_content("Litfit")
-    expect(page).to have_content("Lamp Shade")
+    expect(page).to have_content(project1.name)
+    expect(page).to have_content(project1.material)
+    expect(page).to have_content(project1.challenge.theme)
   end
 end
